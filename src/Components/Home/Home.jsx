a205f171978dfd053fc657fe5417a3e9c693244a
giovanni nady <jovany.nady25@gmail.com>
2024-01-24 13:16:53 +0200

    giovanni

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Home() {
 
-  
+
 
   const backgroundImageStyle = {
     backgroundImage: `url(${process.env.PUBLIC_URL}/bcc.jpg)`,
@@ -16,21 +16,18 @@ export default function Home() {
     padding: '20px', // Adjust padding as needed
     borderRadius: '10px'
   };
-  
-  
+
+
 
   return <>
-  <div style={backgroundImageStyle}>
-    
-       <div className="container d-flex align-items-center justify-content-center vh-100">
-      <div className="text-center">
-        <h1 className='text-white'>Hello I'm Giovanni</h1>
-        <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, ipsum.</p>
-        <button className="btn rounded p-2" style={{ backgroundColor: '#ff7f50', color: '#fff' }} >Check It</button>
+    <div style={backgroundImageStyle}>
+      <div className="container d-flex align-items-center justify-content-center vh-100">
+        <div className="text-center">
+          <h1 className='text-white'>Hello I'm Giovanni</h1>
+          <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, ipsum.</p>
+          <button className="btn rounded p-2" style={{ backgroundColor: '#ff7f50', color: '#fff' }} >Check It</button>
+        </div>
       </div>
     </div>
-    
-    </div>
-  
   </>
 }
