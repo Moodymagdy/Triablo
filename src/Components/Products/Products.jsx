6cd5b9ddc803fbac7272e2759ceb455c79de3705
Moody Magdy <moodymagdy08@gmail.com>
2024-01-27 23:49:51 +0200

    cardproducts

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function BasicExample() {
   const cardData = [
@@ -8,11 +8,9 @@ function BasicExample() {
     { title: 'Blue Scarab Pyramids View', location: 'Dahab', duration: '4 days 3 nights', date: '25 Dec 2023 to 29 Dec 2023', discountedPrice: 'EGP 600', currentPrice: 'EGP 500', rating: 4.8, imageSrc: './hotel.jpg' },
   ];
 
-  const [isHeartClicked, setIsHeartClicked] = useState(false);
+  
 
-  const toggleHeartColor = () => {
-    setIsHeartClicked((prev) => !prev);
-  };
+ 
 
   return (
     <section id='prod'>
@@ -23,24 +21,27 @@ function BasicExample() {
         <div className="row" style={{margin:'70px'}}>
           {cardData.map((card, index) => (
             <div key={index} className="col-3" >
-              <div className="card" style={{ position: 'relative', borderRadius: '60px', width:'230px',height:'320px' }}>
-                <button style={{ position: 'absolute', top: '5px', left: '7px', background: 'transparent', border: 'none', fontSize: '1.2rem', color: isHeartClicked ? 'red' : 'white' }} >   &#10084;  </button>
-                <div className="rating-badge" style={{ position: 'absolute', top: '5px', right: '20px', background: 'orange', color: 'white', padding: '0px', borderRadius: '5px' }}>
+              <div  style={{ position: 'relative', borderRadius: '60px', width:'230px',height:'320px' }}>
+                <button style={{ position: 'absolute', top: '5px', left: '7px', background: 'transparent', border: 'none', fontSize: '1.2rem'}} >   &#10084;  </button>
+                <div className="rating-badge text-center" style={{ position: 'absolute', top: '8px', right: '20px', background: 'orange', color: 'white', padding: '0px', borderRadius: '5px', width:'35px' }}>
                   {card.rating}
                 </div>
                 <div className="card-img-wrapper" style={{ overflow: 'hidden', borderRadius: '40px 40px 0 0' }}>
                   <img src={card.imageSrc} className="card-img-top rounded" alt={`Image for ${card.title}`} />
                 </div>
                 <div className="card-body d-flex flex-column justify-content-between text-left">
+                <div className="vertical-line left-line"></div>
+                  <div className="vertical-line right-line"></div>
                   <div>
-                    <h5 className="card-title" style={{ color: 'orange', fontWeight: 'bold', fontSize: '0.8rem', lineHeight: '1' }}>{card.title}</h5>
-                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0' }}>{card.location}</p>
-                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0' }}>{card.duration}</p>
-                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0' }}>{card.date}</p>
+                    <h5 className="card-title" style={{ color: 'orange', fontWeight: 'bold', fontSize: '0.8rem', lineHeight: '2.2',marginLeft:'9px' }}>{card.title}</h5>
+                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0',marginLeft:'9px' }}>{card.location}</p>
+                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0',marginLeft:'9px' }}>{card.duration}</p>
+                    <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '0',marginLeft:'9px' }}>{card.date}</p>
                     <div style={{ background: 'transparent', marginBottom: '5px' }}>
                     </div>
+                  
                   </div>
-                  <div style={{ background: 'orange', padding: '10px', borderRadius: '0 0 15px 15px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto' }}>
+                  <div style={{ background: 'orange', padding: '10px', borderRadius: '0 0 40px 40px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto' }}>
                     <div style={{ background: 'transparent', marginBottom: '5px' }}>
                       <p className="card-text" style={{ fontWeight: 'bold', fontSize: '0.7rem', lineHeight: '2' }}>
                         <del>{card.discountedPrice}</del> {card.currentPrice}
